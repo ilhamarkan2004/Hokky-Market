66dbd859a65669775b338b1b0e261b508a5c7022
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2022-12-29 20:31:43 +0700

    Update index.blade.php

--- resources/views/admin/barang/index.blade.php
+++ resources/views/admin/barang/index.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout class="overflow-x-hidden">
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <x-slot name="header" class="bg-[#0D0D0D] ">
+        <h2 class="font-semibold text-xl text-white  leading-tight">
             {{ __('List Barang') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 bg-[#0D0D0D] ">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
